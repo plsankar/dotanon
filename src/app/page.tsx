ad71411c257a56d88cbe6f3117eb337ed8ba1292
Lakshmi Sankar <plsankar1996@gmail.com>
2025-02-14 22:43:22 +0530

    fix: suspense boundary error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Providers from "@/providers";
 import UserQueryForm from "@/components/user-query-form";
 import UserQueryResult from "@/components/user-query-result";
+import { Suspense } from "react";
 
 export default function Home() {
     return (
         <div className="py-10 max-w-xl mx-auto px-5">
             <Providers>
-                <UserQueryForm />
-                <UserQueryResult />
+                <Suspense>
+                    <UserQueryForm />
+                    <UserQueryResult />
+                </Suspense>
             </Providers>
         </div>
     );
